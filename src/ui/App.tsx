a6427ae3a9fb833d94fe565671b527e448c1154d
Abdul-Moiz-i999 <aliblkt@gmail.com>
2025-06-11 19:52:51 +0500

    Add DX Improvement further
    Install npm-run-all and setup it so that electron and react run simultaneously without extra commands

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <p>assa</p>
+        <p>12345</p>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
